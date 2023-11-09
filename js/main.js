81bda259c30d8fa594a993304fb37cb3f1bea26b
Daniel Król <danielkrol90@gmail.com>
2023-11-09 22:13:29 +0100

    first section should be finished

---
+++ js/main.js
@@ -0,0 +1,8 @@
+ // Detect window scroll and update navbar
+ $(window).scroll(function(e){          
+    if($(document).scrollTop() > 120) {
+      $('#navbar').addClass("scroll");
+    } else {
+      $('#navbar').removeClass("scroll");
+    }
+  });
