edcc7c329f93cd2b95a8ff143826240c0152eab4
Daniel Król <danielkrol90@gmail.com>
2024-02-08 22:58:49 +0100

    working on testimonies, and preparing for galllery section

--- js/main.js
+++ js/main.js
@@ -13,22 +13,13 @@ $(document).ready(function(){
     $(".owl-carousel").owlCarousel(
         {
             loop:true,
-            margin:10,
+            items:3,
+            dots:true,
+            dotsEach:3,
             nav:true,
             autoplay:true,
             autoplayTimeout:2000,
             autoplayHoverPause:true,
-            responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:5
-            }
-        }
         }
     );
   });
